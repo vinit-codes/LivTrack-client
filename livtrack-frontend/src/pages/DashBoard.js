5459654150648619316a73ed497e1d107c009a5b
nareshcse89856 <nareshmahapatro89856@gmail.com>
2025-02-04 11:26:56 +0530

    logos inserted

--- livtrack-frontend/src/pages/DashBoard.js
+++ livtrack-frontend/src/pages/DashBoard.js
@@ -6,6 +6,11 @@ import { LineChart, Line, XAxis, YAxis, Tooltip } from "recharts";
 import { Button } from "@mantine/core";
 import { useNavigate } from "react-router-dom";
 import "../styles/dashBoard.css";
+import bloodImg from "./logos/blood1.jpg";
+import cholesterolImg from "./logos/cholesterol.png";
+import eyeImg from "./logos/eye.png";
+import logo from "./logos/logoLivTrack.png";
+import pcosImg from "./logos/pcos.png";
 
 const API_URL = process.env.REACT_APP_API_URL;
 
@@ -87,6 +92,7 @@ export default function Dashboard() {
     <div className="dashboard-container">
       {/* User Info */}
       <div className="dashboard-header">
+        <img src={logo} alt="logo" />
         <h1>Welcome to LivTrack</h1> {/* Replace dynamically with user data */}
         <Button onClick={logout} className="logout-button">
           Logout
@@ -99,28 +105,27 @@ export default function Dashboard() {
           className="dashboard-tile"
           onClick={() => navigate("/cholesterol-metrics")}
         >
-          <img src="\public\cholesterol.png" alt="Tile 1" />
+          <img src={cholesterolImg} alt="Tile 1" />
           <p>Cholesterol</p>
         </div>
         <div
           className="dashboard-tile"
-          onClick={() => navigate("/blood-metrics")}
-        >
-          <img src="\public\blood1.jpg" alt="Tile 2" />
-          <p>Blood Reports</p>
+          onClick={() => navigate("/blood-metrics")}>
+          <img src={bloodImg}alt="Tile 2" />
+          <p>Blood Reports </p>
         </div>
         <div
           className="dashboard-tile"
           onClick={() => navigate("/pcos-metrics")}
         >
-          <img src="[UPLOAD YOUR PNG]" alt="Tile 3" />
+          <img src={pcosImg} alt="Tile 3" />
           <p>PCOS</p>
         </div>
         <div
           className="dashboard-tile"
           onClick={() => navigate("/eye-metrics")}
         >
-          <img src="[UPLOAD YOUR PNG]" alt="Tile 4" />
+          <img src={eyeImg} alt="Tile 4" />
           <p>EYE</p>
         </div>
       </div>
